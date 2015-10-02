1b5acf5f9abbdba45a5d18d51c0f76629ed8ac81
Kisha Richardson <kisha@spunklabs.com>
2015-10-02 15:15:43 -0700

    update crawl details on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Centre.create([{ centre_id: 'hawthorn' }, { centre_id: 'sanfrancisco' }])
+Centre.create([{ centre_id: 'bondijunction' }, { centre_id: 'sanfrancisco' }])
 centre1 = Centre.first
 centre2 = Centre.last
+
 Crawl.create([
-  { centre_id: centre2.id, name: 'New Looks', description: 'Fall 2015, what is in.' },
-  { centre_id: centre1.id, name: 'World Cup', description: '' },
-  { centre_id: centre2.id, name: 'Tasting', description: '' }
+  { centre_id: centre2.id, name: 'Sneaker Showcase', description: 'An exclusive sneak peek ant the rarest kicks on the planet!' },
+  { centre_id: centre1.id, name: 'High Heel Showcase', description: 'An amazing first look at the most beautiful designs of the season.' },
+  { centre_id: centre2.id, name: 'Gadget Showcase', description: 'Private Event showcasing cutting edge electronics' }
 ])
 
 crawl = Crawl.first
+
 Store.create([
   { crawl_id: crawl.id, store_service_id: 42_952 },
   { crawl_id: crawl.id, store_service_id: 18_395 },
